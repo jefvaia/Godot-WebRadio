85d0920cd8aa1dbcc07809ab84999859864f8804
Jefvaia <60438784+jefvaia@users.noreply.github.com>
2025-04-17 21:24:37 +0200

    Added Icons

--- addons/webradio/plugin.gd
+++ addons/webradio/plugin.gd
@@ -2,9 +2,9 @@
 extends EditorPlugin
 
 func _enter_tree() -> void:
-	add_custom_type("WebRadioStreamPlayer", "AudioStreamPlayer", preload("res://addons/webradio/node_types/webradiostreamplayer.gd"), preload("res://icon.svg"))
-	add_custom_type("WebRadioStreamPlayer2D", "AudioStreamPlayer2D", preload("res://addons/webradio/node_types/webradiostreamplayer2d.gd"), preload("res://icon.svg"))
-	add_custom_type("WebRadioStreamPlayer3D", "AudioStreamPlayer3D", preload("res://addons/webradio/node_types/webradiostreamplayer3d.gd"), preload("res://icon.svg"))
+	add_custom_type("WebRadioStreamPlayer", "AudioStreamPlayer", preload("res://addons/webradio/node_types/webradiostreamplayer.gd"), preload("res://addons/webradio/icons/WebRadioStreamPlayer.svg"))
+	add_custom_type("WebRadioStreamPlayer2D", "AudioStreamPlayer2D", preload("res://addons/webradio/node_types/webradiostreamplayer2d.gd"), preload("res://addons/webradio/icons/WebRadioStreamPlayer2D.svg"))
+	add_custom_type("WebRadioStreamPlayer3D", "AudioStreamPlayer3D", preload("res://addons/webradio/node_types/webradiostreamplayer3d.gd"), preload("res://addons/webradio/icons/WebRadioStreamPlayer3D.svg"))
 	add_autoload_singleton("WebRadioStreamHelper", "res://addons/webradio/node_types/radiostreamhelper.gd")
 
 func _exit_tree() -> void:
