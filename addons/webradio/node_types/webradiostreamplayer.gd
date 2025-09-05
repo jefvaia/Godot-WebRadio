2045dfb38006b1c55a0f6601b858636c702cf3db
Jefvaia <60438784+jefvaia@users.noreply.github.com>
2025-09-05 11:46:38 +0200

    Centralize radio buffer queue

--- addons/webradio/node_types/webradiostreamplayer.gd
+++ addons/webradio/node_types/webradiostreamplayer.gd
@@ -3,14 +3,18 @@ class_name WebRadioStreamPlayer
 
 @export var url: String
 
+var _http_instance: HTTPClientInstance
+
 func _ready() -> void:
-	var http_instance = WebRadioStreamHelper.get_radio(url)
-	
-	if http_instance == null:
-		http_instance = WebRadioStreamHelper.add_radio(url)
-	
-	http_instance.buffer_ready.connect(_refresh_stream)
+        self.process_thread_group = Node.PROCESS_THREAD_GROUP_SUB_THREAD
+        _http_instance = WebRadioStreamHelper.get_radio(url)
+
+        if _http_instance == null:
+                _http_instance = WebRadioStreamHelper.add_radio(url)
+
+        finished.connect(_http_instance.player_done)
+        _http_instance.buffer_ready.connect(_refresh_stream)
 
 func _refresh_stream(new_stream: AudioStreamMP3):
-	self.set_deferred("stream", new_stream)
-	self.call_deferred("play", 0)
+        self.set_deferred("stream", new_stream)
+        self.call_deferred("play", 0)
--- addons/webradio/node_types/webradiostreamplayer2d.gd
+++ addons/webradio/node_types/webradiostreamplayer2d.gd
@@ -3,18 +3,18 @@ class_name WebRadioStreamPlayer2D
 
 @export var url: String
 
+var _http_instance: HTTPClientInstance
+
 func _ready() -> void:
-	self.process_thread_group = Node.PROCESS_THREAD_GROUP_SUB_THREAD
-	
-	var http_instance = WebRadioStreamHelper.get_radio(url)
-	
-	if http_instance == null:
-		http_instance = WebRadioStreamHelper.add_radio(url)
-	
-	if http_instance.player_done_connected == false:
-		self.finished.connect(http_instance.player_done)
-		http_instance.player_done_connected = true
-	http_instance.buffer_ready.connect(_refresh_stream)
+        self.process_thread_group = Node.PROCESS_THREAD_GROUP_SUB_THREAD
+
+        _http_instance = WebRadioStreamHelper.get_radio(url)
+
+        if _http_instance == null:
+                _http_instance = WebRadioStreamHelper.add_radio(url)
+
+        self.finished.connect(_http_instance.player_done)
+        _http_instance.buffer_ready.connect(_refresh_stream)
 
 func _refresh_stream(new_stream: AudioStreamMP3):
 	self.set_deferred("stream", new_stream)
